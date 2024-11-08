e78600fa5ffcf7689016db6d54ad7cd9178afff5
brenoaug <breaugustocp@outlook.com>
2024-11-08 16:14:14 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Classe Héroi com atributos nome, idade e tipo
+class heroi{
+  constructor (nome, idade, tipo){
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+  //metodo que retorna o tipo de ataque do héroi
+  tipoAtaque() {
+    switch (this.tipo.toLowerCase()) {//transforma o tipo em minusculo para evitar cair no default, caso escreva com a primeira letra maiuscula
+      case 'mago':
+        return 'Magia';
+      case 'guerreiro':
+        return 'Espada';
+      case 'monge':
+        return 'Artes Marciais';
+      case 'ninja':
+        return 'Shuriken';
+      default:
+        return 'punhos';
+    }
+  }
+  //metodo que imprime a frase final
+  atacar(){
+    console.log(`o ${this.tipo} chamado ${this.nome} de ${this.idade} anos atacou usando ${this.tipoAtaque()}`);
+  }
+}
+
+//instanciando 2 herois
+let heroi1 = new heroi('Breno', 27, 'Mago');//eu mesmo, tem q ser mágico pra conseguir trabalhar, namorar, estudar, jogar e treinar(assumo q o ultimo não tenho feito muito)
+let heroi2 = new heroi('Arthur Morgan', 30, 'Pistoleiro');//eu amo Red Dead 😊
+
+heroi1.atacar();
+heroi2.atacar();
